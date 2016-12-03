11cdedbe254c409ab700c6aef49a23df618f2639
Erick Martínez <erick.e_274@hotmail.com>
2016-12-03 13:39:11 -0600

    Add employee model

---
+++ models/Employee.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var employeeSchema = new Schema({
+	employee_number: Number,
+	name: String,
+	last_name: String,
+	rfc: String,
+	gender: String,
+	birth_date: Date,
+	phone_number: Number,
+	email: String,
+	employment: String,
+	hire_date: Date,
+	salary: Number
+});
+
+module.exports = mongoose.model('Employee', productSchema);
